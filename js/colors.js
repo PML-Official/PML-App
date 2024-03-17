b45a91915b524ce9e6ad91e894caad1d63e00474
Stashey13 <161401271+Stashey13@users.noreply.github.com>
2024-03-17 16:59:37 -0400

    adding a few colors to colors.js

--- js/colors.js
+++ js/colors.js
@@ -9,4 +9,6 @@ class Color {
 
 const black = new Color("black", 0, 0, 0);
 const white = new Color("white", 255, 255, 255);
-const blue = new Color("blue", 0, 0, 255);
+const richblue = new Color("rich-blue", 0, 0, 255);
+const richgreen = new Color("rich-green", 0, 255, 0);
+const richred = new Color("rich-red", 255, 0, 0);
