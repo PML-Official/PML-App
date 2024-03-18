c6105adbb0cb15aefb2a00e926807e2ffc6bc7e3
Stashey13 <161401271+Stashey13@users.noreply.github.com>
2024-03-18 16:13:59 -0400

    Adding Icons to App
    please note: haven't tested yet because electron hasn't been tested yet.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 //import { PDFDocument, StandardFonts, rgb } from 'pdf-lib';
 
 const { app, BrowserWindow, shell, globalShortcut, dialog } = require('electron');
+const path = require('path');
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -10,14 +11,17 @@ function createWindow() {
       nodeIntegration: true, // Enable Node.js integration in renderer process
       contextIsolation: false,
     },
+    // Icon Programmability
+    icon: process.platform === 'win32' ? path.join(__dirname, 'Windows Icon.ico') : path.join(__dirname, 'Mac-Icon.icns')
   });
-  win.openDevTools();
+
+  win.webContents.openDevTools();
+
   win.webContents.on('will-navigate', (event, url) => {
     event.preventDefault(); // Prevent default navigation behavior
     shell.openExternal(url); // Open the link in the default system browser
   });
 
-  
   win.loadFile('index.html');
 }
 
@@ -35,14 +39,13 @@ app.on('activate', () => {
   }
 });
 
-
-// Keyboard shortcuts
-
-// ctrl/cmnd Q
-
+// Create window when app is ready
 app.whenReady().then(() => {
   createWindow();
 
+  // Keyboard shortcuts
+
+  // ctrl/cmnd Q
   globalShortcut.register('CommandOrControl+Q', () => {
     console.log('CommandOrControl+Q was pressed');
     dialog.showMessageBox({
@@ -52,26 +55,12 @@ app.whenReady().then(() => {
       buttons: ['OK']
     });
   });
-});
 
-// ctrl/cmnd H
-
-app.whenReady().then(() => {
-  createWindow();
-
+  // ctrl/cmnd H
   globalShortcut.register('CommandOrControl+H', () => {
     console.log('CommandOrControl+H was pressed');
   });
-
-  mainWindow.loadURL(`file://${path.join(__dirname,'../../Desktop/PML-App/index.html')}`)
-
 });
-
-
-
-
-
-
 
 // Unregister all shortcuts when the app is about to quit
 app.on('will-quit', () => {
