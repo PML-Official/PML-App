3032a5e0224f793f0df24020c35635567b0b7e32
Stashey13 <161401271+Stashey13@users.noreply.github.com>
2024-03-18 18:27:36 -0400

    Adding Command Q (opens app version information)

--- main.js
+++ main.js
@@ -1,68 +1,96 @@
 //import { PDFDocument, StandardFonts, rgb } from 'pdf-lib';
 
-const { app, BrowserWindow, shell, globalShortcut, dialog } = require('electron');
+const { app, BrowserWindow, shell, globalShortcut, dialog, ipcMain } = require('electron');
 const path = require('path');
 
+let mainWindow;
+
 function createWindow() {
-  const win = new BrowserWindow({
-    width: 1600,
-    height: 1000,
-    webPreferences: {
-      nodeIntegration: true, // Enable Node.js integration in renderer process
-      contextIsolation: false,
-    },
-    // Icon Programmability
-    icon: process.platform === 'win32' ? path.join(__dirname, 'Windows Icon.ico') : path.join(__dirname, 'Mac-Icon.icns')
-  });
+    mainWindow = new BrowserWindow({
+        width: 1600,
+        height: 1000,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            preload: path.join(__dirname, 'preload.js')
+        },
+        icon: process.platform === 'win32' ? path.join(__dirname, 'Windows Icon.ico') : path.join(__dirname, 'Mac-Icon.icns')
+    });
 
-  win.webContents.openDevTools();
+    mainWindow.webContents.openDevTools();
 
-  win.webContents.on('will-navigate', (event, url) => {
-    event.preventDefault(); // Prevent default navigation behavior
-    shell.openExternal(url); // Open the link in the default system browser
-  });
+    mainWindow.webContents.on('will-navigate', (event, url) => {
+        event.preventDefault();
+        shell.openExternal(url);
+    });
 
-  win.loadFile('index.html');
+    mainWindow.loadFile('index.html');
+
+    // Return mainWindow instance
+    return mainWindow;
 }
 
 // Quit app when all windows are closed
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit();
+      app.quit();
   }
 });
 
-// Create window once app is opened
+// Create window once app is activated (for macOS)
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
+      createWindow();
   }
 });
 
-// Create window when app is ready
-app.whenReady().then(() => {
-  createWindow();
 
-  // Keyboard shortcuts
 
-  // ctrl/cmnd Q
-  globalShortcut.register('CommandOrControl+Q', () => {
-    console.log('CommandOrControl+Q was pressed');
-    dialog.showMessageBox({
-      type: 'error',
-      message: 'Keyboard Shortcut Pressed',
-      detail: 'Command Q was pressed.',
-      buttons: ['OK']
+
+
+
+
+
+
+// Keyboard Shortcuts
+
+
+
+
+
+// Command + Q
+app.on('ready', () => {
+    // Call createWindow function to create the main window
+    mainWindow = createWindow();
+
+    // Register global shortcut
+    const ret = globalShortcut.register('CommandOrControl+Q', () => {
+        console.log('CommandOrControl+Q was pressed');
+        if (mainWindow) {
+            mainWindow.webContents.send('togglePageVisibilityTest');
+            
+        }
     });
-  });
 
-  // ctrl/cmnd H
-  globalShortcut.register('CommandOrControl+H', () => {
-    console.log('CommandOrControl+H was pressed');
-  });
+    if (!ret) {
+        console.log('Registration of Keyboard Shortcuts failed');
+    }
+
+    console.log(globalShortcut.isRegistered('CommandOrControl+Q'));
 });
+
+
+
+
+
+
+
+
 
 // Unregister all shortcuts when the app is about to quit
+
 app.on('will-quit', () => {
-  globalShortcut.unregisterAll();
+    globalShortcut.unregisterAll();
 });
+
+
